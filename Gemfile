3efc29ce087500d9f4efe38c4130c540824e2660
Eduardo Estrada <guayisimo5@gmail.com>
2017-11-21 12:20:04 +0800

    Gemfile update

--- Gemfile
+++ Gemfile
@@ -3,31 +3,31 @@
 source 'https://rubygems.org'
 ruby '2.4.2'
 
-# Networking gems
-gem 'bootstrap', '~> 4.0.0.beta2'
+# infrastructure related
 gem 'http'
 
 # Development/Debugging related
 gem 'pry' # to run console in production
 gem 'rake' # to run console in production
 
-# Web app related
+# Web related
 gem 'econfig'
 gem 'puma'
+gem 'rack-flash3'
 gem 'roda'
 gem 'slim'
 
-# Representers
+# Application related
+gem 'dry-validation'
 gem 'multi_json'
 gem 'roar'
 
 group :test do
+  gem 'headless'
   gem 'minitest'
   gem 'minitest-rg'
   gem 'rack-test'
-  gem 'simplecov'
-  gem 'vcr'
-  gem 'webmock'
+  gem 'watir'
 end
 
 group :development, :test do
--- application/app.rb
+++ application/app.rb
@@ -1,22 +1,29 @@
 # frozen_string_literal: true
 
 require 'roda'
+# require 'slim'
+# require 'slim/include'
 
 module RecipeBuddy
   # Web App
   class App < Roda
     plugin :render, engine: 'slim', views: 'presentation/views'
     plugin :assets, css: 'style.css', path: 'presentation/assets'
+    plugin :flash
+
+    # use Rack::Session::Cookie, secret: config.SESSION_SECRET
 
     route do |routing|
       routing.assets
-      app = App
 
       # GET / request
       routing.root do
         recipes_json = ApiGateway.new.all_recipes
         all_recipes = RecipeBuddy::RecipesRepresenter.new(OpenStruct.new)
                                                      .from_json recipes_json
+        # if all_recipes.recipes.count.zero?
+        #   flash.now[:notice] = 'Add a Facebook page/group to get started'
+        # end
         view 'home', locals: { recipes: all_recipes.recipes }
       end
 
