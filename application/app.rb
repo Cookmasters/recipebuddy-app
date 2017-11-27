d13f129eae960ae2aeb026713057d508a30efdb8
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-11-27 10:45:04 +0800

    Controller refactored to get the best recipes from API

--- application/app.rb
+++ application/app.rb
@@ -18,28 +18,39 @@ module RecipeBuddy
 
       # GET / request
       routing.root do
-        recipes_json = ApiGateway.new.all_recipes
-        all_recipes = RecipeBuddy::RecipesRepresenter.new(OpenStruct.new)
-                                                     .from_json recipes_json
+        recipes_json = ApiGateway.new.best_recipes
+        best_recipes = RecipeBuddy::RecipesRepresenter.new(OpenStruct.new)
+                                                      .from_json recipes_json
 
-        recipes = Views::AllRecipes.new(all_recipes)
+        recipes = Views::AllRecipes.new(best_recipes)
         if recipes.none?
-          flash.now[:notice] = 'Add a Facebook public page/group to get started'
+          flash.now[:notice] = 'Add a Facebook public page to get started'
         end
         view 'home', locals: { recipes: recipes }
       end
 
       routing.on 'page' do
+        # routing.is String do |pagename|
+        #   # GET /api/v0.1/page/:pagename request
+        #   page_json = ApiGateway.new.get_page(pagename)
+        #   page = RecipeBuddy::PageRepresenter.new(OpenStruct.new)
+        #                                      .from_json page_json
+        #
+        #   view_page = Views::Page.new(page)
+        #   view 'page', locals: { page: view_page }
+        # end
+
         routing.post do
-          create_request = Forms::FacebookNameValidator.call(routing.params)
+          create_request = Forms::FacebookPageURLValidator.call(routing.params)
           result = AddPage.new.call(create_request)
+          # page = RecipeBuddy::PageRepresenter.new(OpenStruct.new)
+          #                                    .from_json result.value[:response]
 
           if result.success?
-            flash[:notice] = 'New Facebook Page/group added!'
+            flash[:notice] = 'New Facebook Page added!'
           else
             flash[:error] = result.value
           end
-
           routing.redirect '/'
         end
       end
