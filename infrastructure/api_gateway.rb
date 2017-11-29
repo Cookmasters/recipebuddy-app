65724d84d76e7092d7ad8486fb288e7fd4421589
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-11-29 10:17:50 +0800

    API_URL renamed

--- infrastructure/api_gateway.rb
+++ infrastructure/api_gateway.rb
@@ -34,7 +34,7 @@ module RecipeBuddy
     end
 
     def call_api(method, resources)
-      url_route = [@config.api_url, resources].flatten.join '/'
+      url_route = [@config.API_URL, resources].flatten.join '/'
       result = HTTP.send(method, url_route)
       raise(result.parse['message']) if result.code >= 300
       result.to_s
